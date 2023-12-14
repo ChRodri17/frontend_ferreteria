aa1e3a7e08ba398ca579b5c55759c04e3e4a0b3a
ChRodri17 <chanelrodrigo633@gmail.com>
2023-12-14 14:14:37 -0400

    Modificaciones en Header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,7 @@ const authStore = useAuthStore();
                         <v-container fluid>
                             <img src="../assets/images/logo.png" alt="">
                         </v-container>
-                         <em>FERROTODO</em>
+                        <em>FERROTODO</em>
                     </h2>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
@@ -23,67 +23,63 @@ const authStore = useAuthStore();
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
-                        <!-- <li class="nav-item ">
-                            <a class="nav-link" href="/">Inicio<span class="sr-only">(current)</span>
-                            </a>
-                        </li> -->
+
                         <li class="nav-item">
                             <a class="nav-link click-scroll" href="/">Inicio</a>
                         </li>
 
                         <li class="nav-item">
-                            <RouterLink to="/productos" class="nav-link click-scroll">Productos</RouterLink>
-                        </li>
-
-                        <li v-if="authStore.token" class="nav-item">
-                            <RouterLink to="/categorias" class="nav-link click-scroll">Categoria</RouterLink>
-                        </li>
-
-                        <li v-if="authStore.token" class="nav-item">
-                            <RouterLink to="/unidades" class="nav-link click-scroll">Unidad</RouterLink>
-                        </li>
-
-                        <li v-if="authStore.token" class="nav-item">
-                            <RouterLink to="/ventas" class="nav-link click-scroll">Ventas</RouterLink>
-                        </li>
-
-                        <!-- <li v-if="authStore.token" class="nav-item">
-                            <RouterLink to="/clientes" class="nav-link click-scroll">Cliente</RouterLink>
-                        </li>
-
-                        <li v-if="authStore.token" class="nav-item">
-                            <RouterLink to="/usuarios" class="nav-link click-scroll">Usuario</RouterLink>
-                        </li> -->
-
-                        <li class="nav-item">
                             <a class="nav-link" href="/contacto">Contacto</a>
                         </li>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Admin
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li>
-                                    <RouterLink to="/usuarios" class="nav-link click-scroll" style="color: black;">Usuario
-                                    </RouterLink>
-                                </li>
-                                <li>
-                                    <RouterLink to="/clientes" class="nav-link click-scroll" style="color: black;">Cliente
-                                    </RouterLink>
-                                </li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <!-- <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                            </ul>
-                        </li>
-
                         <li class="nav-item">
-                            <a v-if="!authStore.token" class="nav-link" href="/login">Iniciar Sesion</a>
-                            <a v-else @click="authStore.logout()" class="nav-link">Cerrar Sesión</a>
+                            <RouterLink to="/productos" class="nav-link click-scroll">Productos</RouterLink>
                         </li>
+                        <slot v-if="authStore.token">
+                            <li v-if="authStore.token" class="nav-item">
+                                <RouterLink to="/categorias" class="nav-link click-scroll">Categoria</RouterLink>
+                            </li>
+
+                            <li v-if="authStore.token" class="nav-item">
+                                <RouterLink to="/unidades" class="nav-link click-scroll">Unidad</RouterLink>
+                            </li>
+
+                            <li v-if="authStore.token" class="nav-item">
+                                <RouterLink to="/ventas" class="nav-link click-scroll">Ventas</RouterLink>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Admin
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li>
+                                        <RouterLink to="/usuarios" class="nav-link click-scroll" style="color: black;">
+                                            Usuario
+                                        </RouterLink>
+                                    </li>
+                                    <li>
+                                        <RouterLink to="/clientes" class="nav-link click-scroll" style="color: black;">
+                                            Cliente
+                                        </RouterLink>
+                                    </li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <!-- <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+                                </ul>
+                            </li>
+                        </slot>
+
+                        <RouterLink v-if="!authStore.token" to="/login" class="btn custom-btn d-lg-block d-none" style="color:  white;">Iniciar
+                            Sesión</RouterLink>
+                        <a v-else @click="authStore.logout()" class="btn custom-btn d-lg-block d-none">Salir</a>
+
+                        <!-- <li class="nav-item">
+                            <a v-if="!authStore.token" class="nav-link" href="/views/login">Iniciar Sesion</a>
+                            <a v-else @click="authStore.logout()" class="nav-link">Cerrar Sesión</a>
+                        </li> -->
                     </ul>
                 </div>
             </div>
